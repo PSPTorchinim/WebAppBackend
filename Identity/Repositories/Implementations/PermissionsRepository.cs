ee7ffc30fd89ee77a2d3eae88d651f8cb390dae6
Hubert Troc <huberttroc@gmail.com>
2023-03-08 16:24:01 +0100

    repositories

---
+++ Identity/Repositories/Implementations/PermissionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Handlers.Repositories;
+using Identity.Entities;
+using Identity.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Repositories.Implementations
+{
+    public class PermissionsRepository : Repository<Permission>, IPermissionsRepository
+    {
+        public PermissionsRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
